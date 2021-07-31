e2c4cd695924ade4fdf18249b3188cd6f0ef8d37
snishant011 <shresthanishant@gmail.com>
2021-07-31 14:19:38 +0545

    form successfully added

--- components/newsletter_registor.js
+++ components/newsletter_registor.js
@@ -1,19 +1,31 @@
+import { useForm } from '@formspree/react'
+const Newsletterform = () => {
+  const [state, handleSubmit] = useForm('mqkwyqgq')
+  if (state.succeeded) {
+    return <p>Thanks for subscribing. We will notify for you about offers.!!</p>
+  }
 
+  return (
+    <form className='form-div' onSubmit={handleSubmit} method='post'>
+      <input
+        type='email'
+        name='email'
+        id='email'
+        placeholder='Your Email Here'
+        autoComplete='off'
+        required
+      />
+      <button type='submit' disabled={state.submitting}>
+        Subscribe
+      </button>
+    </form>
+  )
+}
 const NewsletterResistor = () => {
   return (
     <section className='newsletter-registor'>
       <h1>Subscribe to our Newsletter?</h1>
-      <form className='form-div' action='https://facebook.com' method='post'>
-        <input
-          type='email'
-          name='email'
-          id='email'
-          placeholder='Your Email Here'
-          autoComplete='off'
-          required
-        />
-        <button type='submit'>Subscribe</button>
-      </form>
+      <Newsletterform />
     </section>
   )
 }
--- pages/course_request.js
+++ pages/course_request.js
@@ -1,7 +1,63 @@
 import Nav from '../components/nav'
 import MetaHeading from '../components/meta-heading'
+import { useForm } from '@formspree/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+const RequestForm = () => {
+  const [state, handleSubmit] = useForm('xnqlgdoq')
+  if (state.succeeded) {
+    return (
+      <p>
+        We will look for your request.!!
+        <br />
+        Stay tunned.
+      </p>
+    )
+  }
+  return (
+    <form className='form-container' onSubmit={handleSubmit} method='post'>
+      <input
+        type='text'
+        name='name'
+        id='name'
+        placeholder='your name here'
+        autoComplete='off'
+        required
+      />
+      <label htmlFor='name' className='field-label'>
+        Name
+      </label>
+      <input
+        type='email'
+        placeholder='Your Email Here'
+        name='email'
+        id='email'
+        autoComplete='off'
+        required
+      />
+      <label htmlFor='email' className='field-label'>
+        Email
+      </label>
+      <input
+        type='text'
+        placeholder='Course'
+        name='course_name'
+        id='course_name'
+        autoComplete='off'
+        required
+      />
+      <label htmlFor='course_name' className='field-label'>
+        Course/ Book name
+      </label>
+      <button type='submit' disabled={state.submitting} className='btn warrow'>
+        <span id='button-text'>Request Course</span>
+        <div className='icon-container'>
+          <FontAwesomeIcon icon={faArrowRight} className='icon' />
+        </div>
+      </button>
+    </form>
+  )
+}
 const CourseRequestPage = () => {
   return (
     <>
@@ -10,53 +66,8 @@ const CourseRequestPage = () => {
       <div className='course-request-page'>
         <h1>Course/ Book Request Form</h1>
         <div className='course-request-form'>
-          <form
-            className='form-container'
-            action='https://send.pageclip.co/P3QlFYBhB51datXAWbvH1U7dg2Ws9GR3/Course_Request_Form'
-            method='post'
-          >
-            <input
-              type='text'
-              name='name'
-              id='name'
-              placeholder='your name here'
-              autoComplete='off'
-              required
-            />
-            <label htmlFor='name' className='field-label'>
-              Name
-            </label>
-            <input
-              type='email'
-              placeholder='Your Email Here'
-              name='email'
-              id='email'
-              autoComplete='off'
-              required
-            />
-            <label htmlFor='email' className='field-label'>
-              Email
-            </label>
-            <input
-              type='text'
-              placeholder='Course Name'
-              name='course_name'
-              id='course_name'
-              autoComplete='off'
-              required
-            />
-            <label htmlFor='course_name' className='field-label'>
-              Course/ Book name
-            </label>
-            <button type='submit' className='btn warrow'>
-              <span id='button-text'>Request Course</span>
-              <div className='icon-container'>
-                <FontAwesomeIcon icon={faArrowRight} className='icon' />
-              </div>
-            </button>
-          </form>
+          <RequestForm />
         </div>
-        <div id='small-text'>{"Don't worry it's safe"}</div>
       </div>
     </>
   )
