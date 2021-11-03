9b9e32434059bcf98a7fda7dab05ef6bfcfb21f0
Nishant Shrestha <shresthanishant011@gmail.com>
2021-11-03 08:14:55 +0545

    Revert "fixed css bug"
    This reverts commit f3d88a88cc5f9ce4e51e868e25e91f66e3211512.

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,9 +2,7 @@ const Dashboard = () => {
   const user = 'Srest'
   return (
     <>
-      <div className='main'>
-        <h1>Hello, {user}</h1>
-      </div>
+      <h1>Hello, {user}</h1>
     </>
   )
 }
