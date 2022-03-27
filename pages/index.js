4e432041a7a1f02103fca9978586efcab627f965
sNishant011 <shresthanishant011@gmail.com>
2022-03-27 11:29:13 +0545

    added head and icons

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,24 @@ import Courses from '../components/courses'
 import RequestCourseSection from '../components/items/course_request'
 import Testemonials from '../components/testemonials'
 import PlatformFeature from '../components/platform-feature'
+import Head from 'next/head'
 const Home = () => {
   return (
     <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+        />
+        <meta name='description' content='Coden, An e-learning platform.' />
+        <meta name='theme-color' content='#00c997' />
+        <title>CODEN | E-Learning Platform</title>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' href='/apple-icon.png'></link>
+      </Head>
       <Landing />
       <Features />
       <Courses />
