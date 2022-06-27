726bc5c266d84033de4e91f76faf50d30bbd5a1d
nishant <nishant@yarsa.io>
2022-06-27 11:07:52 +0545

    optimized for pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa')
+module.exports = withPWA({
   images: {
     domains: ['storage.googleapis.com'],
   },
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+})
